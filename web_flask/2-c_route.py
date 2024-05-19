a93dda0b2b92d00d1cddb588b0d7af25f9ab729a
vintage254 <derricknjuguna414@gmail.com>
2024-05-19 13:48:39 -0700

    fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,19 +16,19 @@ def hello():
     return 'Hello HBNB!'
 
 @app.route('/hbnb')
-def hello_hbnb():
+def hbnb():
     """
     Return string when route queried
     """
     return 'HBNB'
 
 @app.route('/c/<text>')
-def c_display(text):
+def c_is_fun(text):
     """
     Return string when route queried
     """
     return 'c' + text.replace('_', ' ')
 
-if __name___ = '__main__':
+if __name__ == '__main__':
     app.url_map.strict_slashes=False
     app.run(host = '0.0.0.0', port = '5000')
